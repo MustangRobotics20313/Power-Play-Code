932b04d03bc075386a099650f2797f86eb494ec2
parzivalt37 <sean.mahon78@gmail.com>
2022-11-22 14:41:27 -0600

    FTC dashboard testing and straight camera opmodes

---
+++ Camera.java
@@ -0,0 +1,37 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+
+@Autonomous
+public class Camera extends LinearOpMode {
+    private OpenCvCamera webcam;
+
+    public void runOpMode() {
+        WebcamName webcamName = hardwareMap.get(WebcamName.class, "webcam");
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+
+        webcam = OpenCvCameraFactory.getInstance().createWebcam(webcamName, cameraMonitorViewId);
+
+        webcam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
+            @Override
+            public void onOpened() {
+                webcam.startStreaming(320, 240, OpenCvCameraRotation.UPRIGHT);
+            }
+            @Override
+            public void onError(int errorCode) {}
+        });
+
+        while(!isStarted() && !isStopRequested()) {}
+
+        telemetry.addData("Program is done", cameraMonitorViewId);
+        telemetry.update();
+        sleep(5000);
+
+    }
+}
