4017350ac5676d486fbe4979787b71271a632080
parzivalt37 <parzivalt3719@gmail.com>
2023-01-19 20:16:41 -0600

    added encodertesting class & encoders to teleop

--- AutonomousWithJunction.java
+++ AutonomousWithJunction.java
@@ -4,6 +4,7 @@ import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.Servo;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.openftc.easyopencv.OpenCvCamera;
@@ -21,7 +22,7 @@ public class AutonomousWithJunction extends LinearOpMode {
     private DcMotor fr;
     private DcMotor rl;
     private DcMotor rr;
-    private DcMotor slide;
+    private DcMotorEx slide;
     private Servo grabber;
 
     //camera declarations
@@ -65,14 +66,18 @@ public class AutonomousWithJunction extends LinearOpMode {
         fr = hardwareMap.get(DcMotor.class, "fr");
         rl = hardwareMap.get(DcMotor.class, "rl");
         rr = hardwareMap.get(DcMotor.class, "rr");
-        slide = hardwareMap.get(DcMotor.class, "slide");
+        slide = hardwareMap.get(DcMotorEx.class, "slide");
         grabber = hardwareMap.get(Servo.class, "grabber");
 
+        //actuator configurations
         rr.setDirection(DcMotor.Direction.REVERSE);
         grabber.scaleRange(0, 1);
+        slide.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
 
+        //continually updates the camera analysis while waiting to be started
         while(!isStarted() && !isStopRequested()) {
             telemetry.addData("Analysis: ", pipeline.getAnalysis());
+            telemetry.addData("Slide position: ", slide.getCurrentPosition());
             telemetry.update();
             sleep(1000);
         }
@@ -81,7 +86,14 @@ public class AutonomousWithJunction extends LinearOpMode {
         telemetry.update();
 
         //begin motion code
+        //grab preload cone
         grabber.setPosition(0);
+        while(grabber.getPosition() != 0) { idle(); }
+
+        //barely lift the slide
+        slide.setTargetPosition(100);
+        slide.setPower(0.5);
+
         //strafe left briefly to center the robot in the middle lane
         strafeLeft(0.5, 300);
         //forward (or backward, i guess) down the center lane to reach the middle junction
@@ -93,20 +105,38 @@ public class AutonomousWithJunction extends LinearOpMode {
         allPower(0, 1000);
 
         //raise slide
+        configureSlide();
+        slide.setPower(0.7);
+        while(slide.getCurrentPosition() < slide.getTargetPosition()) {
+            telemetry.addData("Slide position: ", slide.getCurrentPosition());
+            telemetry.update();
+            sleep(100);
+        }
 
+        //inch forward
+        allPower(-0.2, 400);
+        allPower(0, 1500);
+
+        //open servo
+        grabber.setPosition(0.5);
         //retract slide
-
+        allPower(0.2, 400);
+        slide.setMode(DcMotorEx.RunMode.RUN_WITHOUT_ENCODER);
+        slide.setPower(-0.3);
+        while(slide.getCurrentPosition() > 0) { idle(); }
         rotateLeft(rotationPower, rotationTime);
-        allPower(0, 1000);
+        allPower(0, 2000);
 
         switch(sig) {
             case LEFT:
+                //strafes to the right to get into the left section, as robot is still facing backwards
                 strafeRight(strafePower, strafeTime);
                 allPower(0);
                 break;
             case CENTER:
                 break;
             case RIGHT:
+                //strafes to the left to get into the right section, as robot is still facing backwards
                 strafeLeft(strafePower, strafeTime);
                 break;
         }
@@ -129,19 +159,15 @@ public class AutonomousWithJunction extends LinearOpMode {
     }
 
     private void rotateRight(double power, int sleepTime) {
-        fl.setPower(power);
-        rl.setPower(power);
-        fr.setPower(-power);
-        rr.setPower(-power);
-        sleep(sleepTime);
-    }
-
-    private void rotateLeft(double power, int sleepTime) {
         fl.setPower(-power);
         rl.setPower(-power);
         fr.setPower(power);
         rr.setPower(power);
         sleep(sleepTime);
+    }
+
+    private void rotateLeft(double power, int sleepTime) {
+        rotateRight(-power, sleepTime);
     }
 
     private void allPower(double power, int sleepTime) {
@@ -154,5 +180,12 @@ public class AutonomousWithJunction extends LinearOpMode {
         fr.setPower(power);
         rl.setPower(power);
         rr.setPower(power);
+    }
+
+    private void configureSlide() {
+        slide.setTargetPosition(4136);
+        slide.setTargetPositionTolerance(50);
+        slide.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
+
     }
 }
--- GoBildaChassisDrive.java
+++ GoBildaChassisDrive.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.Servo;
 
 @SuppressWarnings({"unused", "FieldCanBeLocal"})
@@ -13,7 +14,7 @@ public class GoBildaChassisDrive extends LinearOpMode {
     private DcMotor fr;
     private DcMotor rl;
     private DcMotor rr;
-    private DcMotor slide;
+    private DcMotorEx slide;
     private Servo grabber;
 
     @Override
@@ -22,10 +23,14 @@ public class GoBildaChassisDrive extends LinearOpMode {
         fr = hardwareMap.get(DcMotor.class, "fr");
         rl = hardwareMap.get(DcMotor.class, "rl");
         rr = hardwareMap.get(DcMotor.class, "rr");
-        slide = hardwareMap.get(DcMotor.class, "slide");
+        slide = hardwareMap.get(DcMotorEx.class, "slide");
         grabber = hardwareMap.get(Servo.class, "grabber");
 
         rr.setDirection(DcMotor.Direction.REVERSE);
+        grabber.scaleRange(0, 1);
+
+
+        slide.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
         waitForStart();
         while(opModeIsActive()) {
@@ -47,13 +52,30 @@ public class GoBildaChassisDrive extends LinearOpMode {
             rl.setPower(gamepad1.right_stick_x);
             rr.setPower(-gamepad1.right_stick_x);
 
-            if (gamepad1.left_bumper) {
-                slide.setPower(0.7);
-            } else if (gamepad1.right_bumper) {
-                slide.setPower(-0.2);
-            } else {
+            if (gamepad2.left_bumper) {
+                slide.setTargetPosition(4150);
+                slide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                slide.setPower(0.6);
+            } else if (gamepad2.right_bumper) {
+                slide.setTargetPosition(0);
+                slide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                slide.setPower(-0.4);
+            } else if (gamepad2.left_trigger > 0) {
+                slide.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+                slide.setPower(0.5);
+            } else if (gamepad2.right_trigger > 0) {
+                slide.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+                if (slide.getCurrentPosition() > 0) {
+                    slide.setPower(-0.25);
+                }
+            }
+
+            if (gamepad2.left_trigger == 0 && gamepad2.right_trigger == 0 && slide.getMode() == DcMotor.RunMode.RUN_WITHOUT_ENCODER) {
                 slide.setPower(0);
             }
+
+            telemetry.addData("Slide position: ", slide.getCurrentPosition());
+
 
             if (gamepad1.left_trigger > 0) {
                 grabber.setPosition(0);
@@ -61,6 +83,13 @@ public class GoBildaChassisDrive extends LinearOpMode {
                 grabber.setPosition(0.5);
             }
 
+            if (gamepad2.a) {
+                grabber.setPosition(0);
+            } else if (gamepad2.b) {
+                grabber.setPosition(0.5);
+            }
+
+            telemetry.addData("Grabber position: ", grabber.getPosition());
             telemetry.update();
         }
     }
