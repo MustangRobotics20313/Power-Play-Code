fe55aa23e84af2a5c54b89b3256137420d67ea81
parzivalt37 <parzivalt3719@gmail.com>
2023-02-16 22:27:39 -0600

    updated autonomous to detect apriltags

--- GoBildaChassisDrive.java
+++ GoBildaChassisDrive.java
@@ -5,6 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.TouchSensor;
 
 @SuppressWarnings({"unused", "FieldCanBeLocal"})
 @TeleOp
@@ -16,6 +17,7 @@ public class GoBildaChassisDrive extends LinearOpMode {
     private DcMotor rr;
     private DcMotorEx slide;
     private Servo grabber;
+    private TouchSensor slideSensor;
 
     @Override
     public void runOpMode() {
@@ -25,10 +27,13 @@ public class GoBildaChassisDrive extends LinearOpMode {
         rr = hardwareMap.get(DcMotor.class, "rr");
         slide = hardwareMap.get(DcMotorEx.class, "slide");
         grabber = hardwareMap.get(Servo.class, "grabber");
+        slideSensor = hardwareMap.get(TouchSensor.class, "slideSensor");
 
-        rr.setDirection(DcMotor.Direction.REVERSE);
+        fl.setDirection(DcMotor.Direction.REVERSE);
+        rl.setDirection(DcMotor.Direction.REVERSE);
         grabber.scaleRange(0, 1);
 
+        telemetry.setMsTransmissionInterval(75);
 
         slide.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
@@ -41,40 +46,41 @@ public class GoBildaChassisDrive extends LinearOpMode {
             rr.setPower(gamepad1.left_stick_y);
 
             //left stick: rotation
-            fl.setPower(-gamepad1.left_stick_x);
-            fr.setPower(gamepad1.left_stick_x);
-            rl.setPower(-gamepad1.left_stick_x);
-            rr.setPower(gamepad1.left_stick_x);
+            fl.setPower(gamepad1.left_stick_x);
+            fr.setPower(-gamepad1.left_stick_x);
+            rl.setPower(gamepad1.left_stick_x);
+            rr.setPower(-gamepad1.left_stick_x);
 
             //right stick: strafing
-            fl.setPower(-gamepad1.right_stick_x);
-            fr.setPower(gamepad1.right_stick_x);
-            rl.setPower(gamepad1.right_stick_x);
-            rr.setPower(-gamepad1.right_stick_x);
+            fl.setPower(gamepad1.right_stick_x);
+            fr.setPower(-gamepad1.right_stick_x);
+            rl.setPower(-gamepad1.right_stick_x);
+            rr.setPower(gamepad1.right_stick_x);
 
-            if (gamepad2.left_bumper) {
-                slide.setTargetPosition(4150);
+            if (gamepad1.left_bumper) {
+                slide.setTargetPosition(3700);
                 slide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
                 slide.setPower(0.6);
-            } else if (gamepad2.right_bumper) {
+            } else if (gamepad1.right_bumper) {
                 slide.setTargetPosition(0);
                 slide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                slide.setPower(-0.4);
-            } else if (gamepad2.left_trigger > 0) {
+                slide.setPower(-0.6);
+            } else if (gamepad1.left_trigger > 0) {
                 slide.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
                 slide.setPower(0.5);
-            } else if (gamepad2.right_trigger > 0) {
+            } else if (gamepad1.right_trigger > 0) {
                 slide.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
                 if (slide.getCurrentPosition() > 0) {
                     slide.setPower(-0.25);
                 }
             }
 
-            if (gamepad2.left_trigger == 0 && gamepad2.right_trigger == 0 && slide.getMode() == DcMotor.RunMode.RUN_WITHOUT_ENCODER) {
+            if (gamepad1.left_trigger == 0 && gamepad1.right_trigger == 0 && slide.getMode() == DcMotor.RunMode.RUN_WITHOUT_ENCODER) {
                 slide.setPower(0);
             }
 
-            telemetry.addData("Slide position: ", slide.getCurrentPosition());
+            telemetry.addData("Slide power\t: ", slide.getPower());
+            telemetry.addData("Slide position\t: ", slide.getCurrentPosition());
 
 
             if (gamepad1.left_trigger > 0) {
@@ -89,8 +95,21 @@ public class GoBildaChassisDrive extends LinearOpMode {
                 grabber.setPosition(0.5);
             }
 
+            if (slideSensor.isPressed()) {
+                slide.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+                slide.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+            }
+
             telemetry.addData("Grabber position: ", grabber.getPosition());
+            getMotorTelemetry();
             telemetry.update();
         }
+    }
+
+    private void getMotorTelemetry() {
+        telemetry.addData("Front left power\t: ", fl.getPower());
+        telemetry.addData("Front right power\t: ", fl.getPower());
+        telemetry.addData("Rear left power\t: ", fl.getPower());
+        telemetry.addData("Front left power\t: ", fl.getPower());
     }
 }
--- LinearSlideTest.java
+++ LinearSlideTest.java
@@ -8,19 +8,40 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 @TeleOp
 public class LinearSlideTest extends LinearOpMode {
 
-    DcMotor arm;
+    private DcMotor slide;
     
     @Override
     public void runOpMode() {
-        arm = hardwareMap.get(DcMotor.class, "arm");
-        
+        slide = hardwareMap.get(DcMotor.class, "slide");
+        slide.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        slide.setDirection(DcMotor.Direction.REVERSE);
+
+        telemetry.setMsTransmissionInterval(75);
+
         waitForStart();
         while(opModeIsActive()) {
-            if (gamepad1.left_trigger > 0) {
-                arm.setPower(1.5);
+            if (gamepad1.left_bumper) {
+                slide.setTargetPosition(4000);
+                slide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                slide.setPower(0.6);
+            } else if (gamepad1.right_bumper) {
+                slide.setTargetPosition(0);
+                slide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                slide.setPower(-0.6);
+            } else if (gamepad1.left_trigger > 0) {
+                slide.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+                slide.setPower(0.6);
             } else if (gamepad1.right_trigger > 0) {
-                arm.setPower(-0.5);
+                slide.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+                slide.setPower(-0.6);
             }
+
+            if (gamepad1.left_trigger == 0 && gamepad1.right_trigger == 0 && slide.getMode() == DcMotor.RunMode.RUN_WITHOUT_ENCODER) {
+                slide.setPower(0);
+            }
+
+            telemetry.addData("Slide position: ", slide.getCurrentPosition());
+            telemetry.update();
         }
     }
 }
